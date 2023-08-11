ed42d3555f1bb193d6e51539d947dcda9931ce40
Hao Zhang <hzhangxyz@outlook.com>
2023-08-11 20:31:09 +0800

    Add comment for functions.

--- codegeex.el
+++ codegeex.el
@@ -38,6 +38,11 @@
 (defvar codegeex-top_k 0 "top_k for completion by CodeGeeX")
 
 (defun codegeex-completion-invoke (prefix suffix lang)
+  "Invoke CodeGeeX completion API.
+
+This function will complete code between PREFIX and SUFFIX, which are usually
+the content before cursor and after cursor, and put the result to the current
+buffer. LANG is the programming lanuauge of the code."
   (let* ((url "https://tianqi.aminer.cn/api/v2/multilingual_code_generate_adapt")
          (data (json-encode `((prompt . ,prefix)
                               (suffix . ,suffix)
@@ -68,9 +73,15 @@
          (with-current-buffer parent-buffer
            (insert result))
          (kill-buffer)))
-     `(,(current-buffer)))))
+     `(,(current-buffer))))
+  nil)
 
 (defun codegeex-debug-invoke (prompt lang begin end)
+  "Invoke the codegeex debugger API.
+
+PROMPT is the code content to be debugged. LANG is the programming language name
+of the code. The result will be replaced between BEGIN and END in the current
+buffer."
   (let* ((url "https://tianqi.aminer.cn/api/v2/multilingual_code_bugfix")
          (data (json-encode `((prompt . ,prompt)
                               (n . 1)
@@ -97,9 +108,11 @@
            (delete-region begin end)
            (insert result))
          (kill-buffer)))
-     `(,(current-buffer) ,begin ,end))))
+     `(,(current-buffer) ,begin ,end)))
+  nil)
 
 (defun codegeex-language ()
+  "Return the language of the current buffer."
   (interactive)
   (let* ((name (symbol-name major-mode))
          (name-without-mode (replace-regexp-in-string "-mode" "" name)))
@@ -107,29 +120,41 @@
 
 ;;;###autoload
 (defun codegeex-buffer-completion ()
+  "CodeGeeX buffer completion.
+
+The completion result will be put in the position of cursor directly."
   (interactive)
   (message "CodeGeeX completing")
   (let ((prefix (buffer-substring (point-min) (point)))
         (suffix (buffer-substring (point) (point-max))))
-    (codegeex-completion-invoke prefix suffix (codegeex-language))))
+    (codegeex-completion-invoke prefix suffix (codegeex-language)))
+  nil)
 
 ;;;###autoload
 (defun codegeex-buffer-debug ()
+  "CodeGeeX debugging for the current buffer.
+
+The result will be replaced into the buffer directly."
   (interactive)
   (message "CodeGeeX debugging")
   (let* ((begin (point-min))
          (end (point-max))
          (prompt (buffer-substring begin end)))
-    (codegeex-debug-invoke prompt (codegeex-language) begin end)))
+    (codegeex-debug-invoke prompt (codegeex-language) begin end))
+  nil)
 
 ;;;###autoload
 (defun codegeex-region-debug ()
+  "CodeGeeX debugging of region.
+
+The result will be replaced into the selected region."
   (interactive)
   (message "CodeGeeX debugging")
   (let* ((begin (region-beginning))
          (end (region-end))
          (prompt (buffer-substring begin end)))
-    (codegeex-debug-invoke prompt (codegeex-language) begin end)))
+    (codegeex-debug-invoke prompt (codegeex-language) begin end))
+  nil)
 
 (keymap-global-set "M-\\" 'codegeex-buffer-completion)
 
